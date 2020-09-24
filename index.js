ecd72e2e11ee6f97e668cfdc54e0e11384d40e6f
Natasha-Moon <natashamoon1304@gmail.com>
2020-09-24 16:14:25 +1000

    add in card to display information

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const inputBox = document.getElementById("city-input");
 formInput.addEventListener("submit", (event) => {
   event.preventDefault();
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid={apikey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputBox.value}&appid={api}`
   )
     .then((response) => response.json())
-    .then((data) => {
-      const itemHTML = `
-          <div id="response">
-          <p id="response-card">${data}</p>
-          </div>`;
-      const output = data;
+    .then((output) => {
+      const itemHTML = (name, temp, feelsLike, description) =>
+        `<div class="card" style="width: 18rem">
+      <div class="card-body" id="response-card">
+        <h5 class="card-title">${output.name}</h5>
+        <h3>Temperature ${output.main.temp}</h3>
+        <p class="card-text">Feels like ${output.main.feels_like}
+        <br>
+        ${output.weather[0].description}</p>
+      </div>
+    </div>`;
       console.log(output);
-      responseCard.innerHTML = output.name + output.main.temp;
+      responseCard.innerHTML = itemHTML();
     });
 });
